e7adc30831ce45c67c1f65479c653150f04c56e1
fogcitymarathoner <marc@fogtest.com>
2018-04-15 12:29:09 -0700

    Removed label demo

--- src/main/java/com/fogtest/api/ApiApplication.java
+++ src/main/java/com/fogtest/api/ApiApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.fogtest.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
 @RestController
-public class DemoApplication {
+public class ApiApplication {
   public static void main(String[] args) {
-    SpringApplication.run(DemoApplication.class, args);
+    SpringApplication.run(ApiApplication.class, args);
   }
 
   @GetMapping("/")
