90c7a5d18d2f861c04585d58955d4625a36b6e0f
Marc Condon <marc@fogtest.com>
2018-04-24 20:00:38 -0700

    integrated template engine for MVC development

--- src/main/java/com/fogtest/api/ClientController.java
+++ src/main/java/com/fogtest/api/ClientController.java
@@ -1,6 +1,5 @@
 package com.fogtest.api;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Date;
 
 @Controller
 @RequestMapping("/clients")
