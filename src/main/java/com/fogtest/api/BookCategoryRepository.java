1788eebda54200492fc420f8f9b7c4bc63c95d80
Marc Condon <marc@fogtest.com>
2018-04-30 09:29:56 -0700

    removed training ware

--- src/main/java/com/fogtest/api/BookCategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fogtest.api;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookCategoryRepository extends CrudRepository<BookCategory, Long> {
-
-}
