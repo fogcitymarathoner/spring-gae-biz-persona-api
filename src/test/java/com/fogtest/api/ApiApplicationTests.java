35201155e8ced9c71b685c501d5e46f10a7a6696
Marc Condon <marc@fogtest.com>
2018-04-30 12:42:57 -0700

    added empty setup() and teardown() to starter test, added simple 1+1=2 test

--- src/test/java/com/fogtest/api/ApiApplicationTests.java
+++ src/test/java/com/fogtest/api/ApiApplicationTests.java
@@ -9,8 +9,23 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class ApiApplicationTests {
 
+	@BeforeAll
+	public static void setUp() {
+		// Do something before all tests
+	}
+
+	@AfterAll
+	public static void tearDown() {
+		// Do something after all tests
+	}
+
 	@Test
 	public void contextLoads() {
 	}
 
+	@Test
+	public void testAdd() {
+		int a = 1, b = 1;
+		Assertions.assertEquals(2, a + b);
+	}
 }
