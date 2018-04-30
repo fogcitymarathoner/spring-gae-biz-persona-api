091062477777768769350c5592ba29bd0166d331
Marc Condon <marc@fogtest.com>
2018-04-30 16:50:46 -0700

    more progress on client controller testing, tests title tag render

--- src/test/java/com/fogtest/api/ClientControllerTests.java
+++ src/test/java/com/fogtest/api/ClientControllerTests.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.Test;
 import org.junit.Before;
 import org.junit.After;
+import static org.junit.Assert.assertTrue;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +12,9 @@ import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
@@ -30,8 +34,17 @@ public class ClientControllerTests {
         assertThat(controller).isNotNull();
     }
     @Test
-    public void greetingShouldReturnDefaultMessage() throws Exception {
-        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/",
-                String.class)).contains("Hello World!");
+    public void htmlRender() throws Exception {
+        final String url = "http://localhost:" + port + "/clients";
+        String response = this.restTemplate.getForObject(url,
+                String.class);
+        HttpHeaders httpHeaders = this.restTemplate
+                .headForHeaders(url);
+        System.out.println(httpHeaders.getContentType());
+        assertTrue(httpHeaders.getContentType()
+                .includes(MediaType.TEXT_HTML));
+        System.out.println(response);
+        System.out.println(httpHeaders.get("title"));
+        assertThat(response.contains("Hello World!"));
     }
 }
