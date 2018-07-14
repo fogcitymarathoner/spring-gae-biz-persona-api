39c0526206f4f3646856d85a1fea69c9b6ef41f7
Marc Condon <marc@fogtest.com>
2018-07-14 14:11:16 -0700

    unwrapped invoice in controller for details view

--- src/main/java/com/fogtest/api/InvoiceDetailsController.java
+++ src/main/java/com/fogtest/api/InvoiceDetailsController.java
@@ -25,30 +25,8 @@ public class InvoiceDetailsController {
 
     @GetMapping
     public String getInvoice(@PathVariable Long id, Model model){
-        invoice = this.invoiceServices.getById(id);
-        System.out.println(invoice.toString());
-        System.out.println(invoice.getClass());
-        System.out.println(invoice);
-        invoice.ifPresent(invoice -> {
-            model.addAttribute("invoice", invoice);
-//            model.addAttribute("date", invoice.getDate());
-//            model.addAttribute("po", invoice.getPo());
-//            model.addAttribute("employerexpenserate", invoice.getEmployerexpenserate());
-//            model.addAttribute("terms", invoice.getTerms());
-//            model.addAttribute("timecard", invoice.getTimecard());
-//            model.addAttribute("notes", invoice.getNotes());
-//            model.addAttribute("period_start", invoice.getPeriod_start());
-//            model.addAttribute("period_end", invoice.getPeriod_end());
-//            model.addAttribute("posted", invoice.getPosted());
-//            model.addAttribute("cleared", invoice.getCleared());
-//            model.addAttribute("cleared_date", invoice.getCleared_date());
-//            model.addAttribute("prcleared", invoice.getPrcleared());
-//            model.addAttribute("timecard_receipt_sent", invoice.getAmount());
-//            model.addAttribute("message", invoice.getMessage());
-//            model.addAttribute("amount", invoice.getAmount());
-//            model.addAttribute("voided", invoice.getVoided());
-        });
 
+        this.invoiceServices.getById(id).ifPresent(o -> model.addAttribute("invoice", o));
         return "invoice";
     }
 }
